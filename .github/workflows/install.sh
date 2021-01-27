523638f05d1404ba13b88b50cce9b0bde9f3b533
Yakov Pechersky <ffxen158@gmail.com>
2021-01-26 19:03:12 -0500

    sudo pip

--- .github/workflows/install.sh
+++ .github/workflows/install.sh
@@ -7,7 +7,7 @@ sudo apt-get install -y texlive-extra-utils
 sudo apt-get install -y texlive-xetex
 
 python3 -m pip install --upgrade pip
-pip install --user invoke
-pip install --user mathlibtools
-pip install --user git+https://github.com/plastex/plastex.git
-pip install --user git+https://github.com/PatrickMassot/leanblueprint.git
+sudo pip install invoke
+sudo pip install mathlibtools
+sudo pip install git+https://github.com/plastex/plastex.git
+sudo pip install git+https://github.com/PatrickMassot/leanblueprint.git
