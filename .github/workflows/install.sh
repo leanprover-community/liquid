7711dbd6aa885b268cc91dc1e948601d71e74bc4
Yakov Pechersky <ffxen158@gmail.com>
2021-01-26 19:03:32 -0500

    just try to leanproject

--- .github/workflows/install.sh
+++ .github/workflows/install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -e
 
 sudo apt-get update
-sudo apt-get install -y libgraphviz-dev
-sudo apt-get install -y pdf2svg
-sudo apt-get install -y texlive-extra-utils
-sudo apt-get install -y texlive-xetex
+# sudo apt-get install -y libgraphviz-dev
+# sudo apt-get install -y pdf2svg
+# sudo apt-get install -y texlive-extra-utils
+# sudo apt-get install -y texlive-xetex
 
 python3 -m pip install --upgrade pip
-sudo pip install invoke
+# sudo pip install invoke
 sudo pip install mathlibtools
-sudo pip install git+https://github.com/plastex/plastex.git
-sudo pip install git+https://github.com/PatrickMassot/leanblueprint.git
+# sudo pip install git+https://github.com/plastex/plastex.git
+# sudo pip install git+https://github.com/PatrickMassot/leanblueprint.git
